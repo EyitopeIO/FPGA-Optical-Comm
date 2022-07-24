66d47ed90664918c6c9c798a5f8918e86e7ca6de
EyitopeIO <adeyitope.io@gmail.com>
2022-07-24 17:18:51 +0100

    Update manchester top module for manchester output only

--- manchester_main.vhd
+++ manchester_main.vhd
@@ -6,7 +6,7 @@ USE IEEE.NUMERIC_STD.ALL;
 ENTITY main IS
     GENERIC(
         master_clock        :   NATURAL     := 100_000_000;
-        data_bus_width      :   NATURAL     := 16;
+        data_bus_width      :   NATURAL     := 8;
         ascii_gen_bus_width :   NATURAL     := 8;   --ideally should by related to data_bus_width by formula
         uart_baud_rate      :   INTEGER     := 9600
     );
@@ -15,29 +15,14 @@ ENTITY main IS
         clock:      IN STD_LOGIC;
         reset:      IN STD_LOGIC;
         --ppm_out:    OUT STD_LOGIC;
-        man_out:    OUT STD_LOGIC
-        --ppm_in:     IN STD_LOGIC;
+        man_out:    OUT STD_LOGIC;
+        ---ppm_in:     IN STD_LOGIC;
         --man_in:     IN STD_LOGIC
     );
 END main;
 
+
 ARCHITECTURE monarch OF main is
-    COMPONENT Manchester IS
-        PORT (
-            clk16x :    IN STD_LOGIC;
-            srst :      IN STD_LOGIC;
-            rxd :       IN STD_LOGIC;
-            rx_data :   OUT STD_LOGIC_VECTOR (15 downto 0);
-            rx_stb :    OUT STD_LOGIC;
-            rx_idle :   OUT STD_LOGIC;
-            fm_err :    OUT STD_LOGIC;
-            txd :       OUT STD_LOGIC;
-            tx_data :   IN STD_LOGIC_VECTOR (15 downto 0);
-            tx_stb :    IN STD_LOGIC;
-            tx_idle :   OUT STD_LOGIC;
-            or_err :    OUT STD_LOGIC
-        );
-    END COMPONENT;
 
     COMPONENT ascii_gen IS
         GENERIC (
@@ -49,33 +34,39 @@ ARCHITECTURE monarch OF main is
         );
     END COMPONENT;
 
-    COMPONENT clock_divider IS
+    COMPONENT me IS 
         PORT (
-            clock_100MHz    :   IN STD_lOGIC;
-            clock_1Hz       :   OUT STD_lOGIC
+            rst, clk16x, wrn : STD_LOGIC;
+            din : IN STD_LOGIC_VECTOR(ascii_gen_bus_width-1 DOWNTO 0) ;
+            tbre : OUT STD_LOGIC;
+            mdo : OUT STD_LOGIC
         );
     END COMPONENT;
 
     SIGNAL data_bus_line_for_randomgen:         STD_LOGIC_VECTOR(ascii_gen_bus_width-1 DOWNTO 0);    --allow 0 - 255 random numbers only
-    SIGNAL data_bus_line_for_transmission:      STD_LOGIC_VECTOR(data_bus_width-1 DOWNTO 0);     
 
-    SIGNAL transmit_interval_line:          STD_LOGIC;  --send data whenever this clock changes              
-
-    SIGNAL manchester_transmit_data_line:   STD_LOGIC_VECTOR(data_bus_width-1 DOWNTO 0);
-
-    SIGNAL begin_manchester_transmission:       STD_LOGIC   := '0';    
-
+    SIGNAL manchester_write_control :    STD_LOGIC := '0';
+    SIGNAL manchester_ready_for_data_on_din : STD_LOGIC;
+    SIGNAL data_bus_line_for_transmission:  STD_LOGIC_VECTOR(data_bus_width-1 DOWNTO 0);     
 
 BEGIN
-    
-    data_bus_line_for_transmission(data_bus_width-1 DOWNTO ascii_gen_bus_width) <= (others => '0');
 
-    manchester_transmit_data_line <= data_bus_line_for_transmission;
-
-    PROCESS(clock, data_bus_line_for_randomgen)
+    PROCESS(clock, data_bus_line_for_randomgen)  --Continuous transmission so long as encoder is ready.
+        VARIABLE clock_count : UNSIGNED (data_bus_width-1 DOWNTO 0) := 0 ;
     BEGIN
-        data_bus_line_for_transmission(ascii_gen_bus_width-1 DOWNTO 0) <= data_bus_line_for_randomgen;
-    END PROCESS;
+        IF reset = '0' THEN
+
+            IF manchester_ready_for_data_on_din = '1' THEN
+                clock_count <= 100 ;
+                manchester_write_control <= '1' ;            
+                data_bus_line_for_transmission <= STD_LOGIC_VECTOR(clock_count) ;
+            ELSE
+                manchester_write_control <= '0' ;
+                clock_count <= 0 ;            
+            END IF ;
+    
+        END IF ;
+    END PROCESS ;
 
 
 
@@ -83,33 +74,13 @@ BEGIN
 -------------------------------------  PORT MAPS ------------------------------------------------------
 -------------------------------------------------------------------------------------------------------
 
-CLOCKDIV: clock_divider
-PORT MAP (
-    clock_100MHz => clock,
-    clock_1Hz => transmit_interval_line
-);
 
 DATASRC: ascii_gen
 GENERIC MAP ( data_bus_width => ascii_gen_bus_width )
-PORT MAP (
-    rand => data_bus_line_for_randomgen,
-    clock => transmit_interval_line
-);
+PORT MAP (rand=>data_bus_line_for_randomgen, clock=>clock) ;
 
-MANSTER: Manchester
-PORT MAP (
-    clk16x => clock,
-    srst => reset,
-    rxd => 'U',
-    rx_data => OPEN,
-    rx_stb => OPEN,
-    rx_idle => OPEN,
-    fm_err => OPEN,
-    txd => man_out,
-    tx_data => manchester_transmit_data_line,
-    tx_stb => begin_manchester_transmission,
-    tx_idle => OPEN,
-    or_err => OPEN
-);
+MANENCODE: me
+PORT MAP (rst=>reset, clk16x=>clock, wrn=>manchester_write_control, din=>data_bus_line_for_transmission, tbre=>manchester_ready_for_data_on_dout, mdo=>man_out) ;
+
 
 END monarch;
