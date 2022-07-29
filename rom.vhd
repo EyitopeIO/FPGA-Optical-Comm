689a5e17da513de932d69f2ec1b255e053f9b106
EyitopeIO <adeyitope.io@gmail.com>
2022-07-29 22:13:25 +0100

    Synchronise data soure (ROM) with clock

--- rom.vhd
+++ rom.vhd
@@ -4,25 +4,24 @@ USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY rom IS
     PORT (
-        data_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) ;  --32 bits
-        addr: IN STD_LOGIC_VECTOR(7 DOWNTO 0)
+        data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) ;  --32 bits
+        addr: IN STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
 END rom;
 
 
 ARCHITECTURE romarch OF rom IS
-    TYPE ro_memory IS ARRAY (0 TO 45) OF STD_LOGIC_VECTOR(7 DOWNTO 0) ;
+    TYPE ro_memory IS ARRAY (0 TO 11) OF STD_LOGIC_VECTOR(31 DOWNTO 0) ;
     CONSTANT bytesel : ro_memory := (  
-        --{'RPM':35,'NMD':2900,'SPD':120,'HLS':5,'BP':7}  
-        x"7B", x"27", x"52", x"50", x"4D", x"27", x"3A", x"33", x"35",
-        x"2C", x"27", x"4E", x"4D", x"44", x"27", x"3A", x"32", x"39",       
-        x"30", x"30", x"2C", x"27", x"53", x"50", x"44", x"27", x"3A",   
-        x"31", x"32", x"30", x"2C", x"27", x"48", x"4C", x"53", x"27",
-        x"3A", x"35", x"2C", x"27", x"42", x"50", x"27", x"3A", x"37",
-        x"7D"
+
+        --{'RPM':35,'NMD':2900,'SPD':120,'HLS':5,'BP':7} 
+
+        x"7B275250", x"4D273A33", x"352C274E", x"4D44273A", x"32393030",
+        x"2C275350", x"44273A31", x"32302C27", x"484C5327", x"3A352C27", 
+        x"4250273A", x"377D0000"
     );
 BEGIN
-    data_out <= bytesel(TO_INTEGER(UNSIGNED(addr))) WHEN UNSIGNED(addr) < 46 ELSE x"FF" ;
+    data_out <= bytesel(TO_INTEGER(UNSIGNED(addr))) WHEN UNSIGNED(addr) < 12 ELSE x"FFFFFFFF" ;
 END romarch ;
                             
 
